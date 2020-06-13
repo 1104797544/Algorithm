b963f149bcd0d0e0bee56ea1e43fbb4d551c7ec6
JRS <1104797544@qq.com>
2020-06-13 18:24:00 +0800

    数据结构与算法之美练习--链表

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-import com.jzf.datastructure.*;
-import com.jzf.leetcode.LongestCommonPrefix;
-import com.jzf.leetcode.ReverseInteger;
-import com.jzf.other.Merger;
-
-import java.util.PriorityQueue;
-import java.util.Random;
-import java.util.Set;
-import java.util.Stack;
+import com.jzf.datastructure.beauty.basic.SingleLinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        SingleLinkedList<Integer> singleLinkedList = new SingleLinkedList<>();
+        singleLinkedList.add(1);
+        singleLinkedList.add(2);
+        singleLinkedList.add(3);
+
+        singleLinkedList.contains(5);
+
 
 /*        Integer[] nums = {-2,0,3,-5,2,1};
         SegmentTree<Integer> segmentTree = new SegmentTree<>(nums, new Merger<Integer>() {
@@ -27,9 +27,9 @@ public class Main {
 /*        ReverseInteger reverseInteger = new ReverseInteger();
         int a = reverseInteger.reverse(1534236469);
         System.out.println(a);*/
-        String[] strs = {"c", "c"};
+/*        String[] strs = {"c", "c"};
         String rs = new LongestCommonPrefix().longestCommonPrefix(strs);
-        System.out.println(rs);
+        System.out.println(rs);*/
 
 
 /*        MaxHeap<Integer> heap = new MaxHeap<>();
